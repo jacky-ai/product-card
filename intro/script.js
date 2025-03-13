a9d575e2fa4605d6a7cbf827734838a2dd303d5b
jacky-ai <pathanjilani48@gmail.com>
2025-03-13 15:45:35 +0530

    second commit

---
+++ intro/script.js
@@ -0,0 +1,13 @@
+alert("hello jilani");
+console.log("yeah is running");
+var a = prompt("enter your number");
+var isTrue = confirm("are you sure to delete your data");
+if (isTrue) {
+  console.log("data is deleted");
+} else {
+  console.log("data is not deleted");
+}
+console.log("your number is " + a);
+
+document.title = "hey i am here";
+document.body.style.backgroundColor = "red";
